17e0be36242dcd478bd7b3abe8b1c4e9ff9ba4e8
krisdanieltmg <67691729+krisdanieltmg@users.noreply.github.com>
2021-02-15 18:00:42 +0200

    Add simple FpsController

---
+++ Ninja-Puzzle/Assets/NinjaPuzzle/Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace NinjaPuzzle.Scripts
+{
+	public class MouseLook : MonoBehaviour
+	{
+		[SerializeField] Transform playerBody;
+		[SerializeField] float mouseSensitivity = 100f;
+
+		float xRotation = 0;
+	
+		// Start is called before the first frame update
+		void Start()
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+			float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+			xRotation -= mouseY;
+			xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+	    
+			transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+			playerBody.Rotate(Vector3.up * mouseX);
+		}
+	}
+}
